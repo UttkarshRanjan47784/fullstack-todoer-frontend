6cf0d0d301fb8c7cf7e81dd8670652f937627ae7
Uttkarsh Ranjan <utr.1206@gmail.com>
2024-03-30 18:17:22 +0530

    Added Delete Task List Logic

--- src/components/home/TodoListForm.jsx
+++ src/components/home/TodoListForm.jsx
@@ -52,13 +52,51 @@ import { todoListList } from '../../store/atoms';
     }
   }
 
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    if (taskListTitle.length == 0){
+      alert(`Enter Task List Title`);
+      return;
+    }
+    if (taskList.filter((item)=>{ return (item == taskListTitle) }).length != 0){
+      //frontend update (Optimistic Rendering)
+      setTaskList((prev) => {
+        let arr = prev;
+        arr = arr.filter((item) => { return item != taskListTitle })
+        return [...arr]
+      });
+      try {
+        //backend update
+        let token = localStorage.getItem(`todoer-user-token`)
+        let response = await axios.post(`http://localhost:5000/deletetodolist`, {
+            todoListName : taskListTitle
+          },{
+          headers : {
+            authorization : token
+          }
+        });
+        console.log(response.data)
+      } catch (error) {
+        //rollback frontend change
+        console.log(error.message);
+        alert(`Operation Failed`);
+        setTaskList((prev) => {
+          let arr = prev;
+          arr = [...arr, taskListTitle];
+          return [...arr]
+        });
+      }
+    }
+  }
+
   return (
     <form className='grid grid-cols-5 justify-center items-center bg-slate-500 p-1 gap-1'>
         <input className='text-center col-span-3'
         type='text' placeholder='Task List' value={taskListTitle} onChange={handleTaskListTitle}></input>
         <button type='submit' className='bg-orange-400 hover:bg-orange-500 py-1 px-3 rounded-full'
         onClick={handleAdd}>ADD</button>
-        <button className='bg-orange-400 hover:bg-orange-500 py-1 px-3 rounded-full'>SEARCH</button>
+        <button className='bg-orange-400 hover:bg-orange-500 py-1 px-3 rounded-full'
+        onClick={handleDelete}>DELETE</button>
     </form>
   )
  });
